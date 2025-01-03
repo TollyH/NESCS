f5b2d212b9e782a65106e56afa7380a40dd59dbf
TollyH <ptolemyhill@sky.com>
2025-01-03 23:38:36 +0000

    Define registers and status flags

---
+++ NESCS/Registers.cs
@@ -0,0 +1,27 @@
+﻿namespace NESCS
+{
+    [Flags]
+    public enum StatusFlags : byte
+    {
+        None = 0,
+
+        Carry = 0b1,
+        Zero = 0b10,
+        InterruptDisable = 0b100,
+        Decimal = 0b1000,
+        BreakFlag = 0b10000,
+        Always = 0b100000,
+        Overflow = 0b1000000,
+        Negative = 0b10000000
+    }
+
+    public record Registers
+    {
+        public byte A { get; set; }
+        public byte X { get; set; }
+        public byte Y { get; set; }
+        public ushort PC { get; set; }
+        public byte S { get; set; }
+        public StatusFlags P { get; set; }
+    }
+}
