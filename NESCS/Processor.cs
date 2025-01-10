fad1cd7b03032dd20854ffb44f7539fd3deb2e15
TollyH <ptolemyhill@sky.com>
2025-01-10 22:30:36 +0000

    Implement first three instructions

--- NESCS/Processor.cs
+++ NESCS/Processor.cs
@@ -22,6 +22,8 @@
 
     public class Processor
     {
+        public const byte SignBit = unchecked((byte)sbyte.MinValue);
+
         public const long NtscClockTicks = 1789773 * TimeSpan.TicksPerSecond;
         public const long PalClockTicks = 1662607 * TimeSpan.TicksPerSecond;
 
@@ -46,47 +48,134 @@
             byte instructionCode = (byte)(opcode >> 5);
 
             AddressingMode addressingMode = GetAddressingMode(instructionGroup, addressingModeCode, instructionCode);
-            ushort operand = ReadOperand(addressingMode);
+
+            switch (instructionGroup)
+            {
+                // Group One
+                case 0b01:
+                    switch (instructionCode)
+                    {
+                        // ORA
+                        case 0b000:
+                            CpuRegisters.A |= ReadOperand(addressingMode);
+                            SetFlagsFromAccumulator();
+                            break;
+                        // AND
+                        case 0b001:
+                            CpuRegisters.A &= ReadOperand(addressingMode);
+                            SetFlagsFromAccumulator();
+                            break;
+                        // EOR
+                        case 0b010:
+                            CpuRegisters.A ^= ReadOperand(addressingMode);
+                            SetFlagsFromAccumulator();
+                            break;
+                    }
+                    break;
+            }
 
             return 0;
         }
 
         /// <summary>
-        /// Read an instruction operand for the given addressing mode at the current PC register.
+        /// Read an instruction operand for the given addressing mode at the current PC register value.
         /// PC will be automatically incremented to the start of the next instruction.
         /// </summary>
         /// <returns>
         /// One of the following:
         /// <list type="bullet">
-        ///     <item><see cref="ushort.MaxValue"/> for addressing modes that don't have an operand.</item>
-        ///     <item>The calculated memory address for addressing modes that read/write from memory.</item>
+        ///     <item>The value at the calculated memory address for addressing modes that read from memory.</item>
         ///     <item>The immediate value for the Immediate addressing mode.</item>
         /// </list>
         /// </returns>
-        public ushort ReadOperand(AddressingMode mode)
+        public byte ReadOperand(AddressingMode mode)
         {
             return mode switch
             {
-                AddressingMode.Accumulator or AddressingMode.Implicit => ushort.MaxValue,
-                AddressingMode.Immediate or AddressingMode.ZeroPage => SystemMemory[CpuRegisters.PC++],
-                AddressingMode.ZeroPageXIndexed => (byte)(SystemMemory[CpuRegisters.PC++] + CpuRegisters.X),
-                AddressingMode.ZeroPageYIndexed => (byte)(SystemMemory[CpuRegisters.PC++] + CpuRegisters.Y),
-                AddressingMode.Relative => (ushort)(SystemMemory[CpuRegisters.PC++] + CpuRegisters.PC),
-                AddressingMode.Absolute => SystemMemory.ReadTwoBytes(CpuRegisters.PC.PostIncrementTwo()),
-                AddressingMode.AbsoluteXIndexed => (ushort)(SystemMemory.ReadTwoBytes(CpuRegisters.PC.PostIncrementTwo()) + CpuRegisters.X),
-                AddressingMode.AbsoluteYIndexed => (ushort)(SystemMemory.ReadTwoBytes(CpuRegisters.PC.PostIncrementTwo()) + CpuRegisters.Y),
-                AddressingMode.Indirect => SystemMemory.ReadTwoBytes(SystemMemory.ReadTwoBytes(CpuRegisters.PC.PostIncrementTwo())),
-                AddressingMode.IndexedIndirect => SystemMemory.ReadTwoBytes((byte)(SystemMemory[CpuRegisters.PC++] + CpuRegisters.X)),
-                AddressingMode.IndirectIndexed => (ushort)(SystemMemory.ReadTwoBytes(SystemMemory[CpuRegisters.PC++]) + CpuRegisters.Y),
-                _ => ushort.MaxValue
+                AddressingMode.Immediate => SystemMemory[CpuRegisters.PC++],
+                AddressingMode.ZeroPage => SystemMemory[SystemMemory[CpuRegisters.PC++]],
+                AddressingMode.ZeroPageXIndexed => SystemMemory[(byte)(SystemMemory[CpuRegisters.PC++] + CpuRegisters.X)],
+                AddressingMode.ZeroPageYIndexed => SystemMemory[(byte)(SystemMemory[CpuRegisters.PC++] + CpuRegisters.Y)],
+                AddressingMode.Relative => SystemMemory[(ushort)(SystemMemory[CpuRegisters.PC++] + CpuRegisters.PC)],
+                AddressingMode.Absolute => SystemMemory[SystemMemory.ReadTwoBytes(CpuRegisters.PC.PostIncrementTwo())],
+                AddressingMode.AbsoluteXIndexed => SystemMemory[(ushort)(SystemMemory.ReadTwoBytes(CpuRegisters.PC.PostIncrementTwo()) + CpuRegisters.X)],
+                AddressingMode.AbsoluteYIndexed => SystemMemory[(ushort)(SystemMemory.ReadTwoBytes(CpuRegisters.PC.PostIncrementTwo()) + CpuRegisters.Y)],
+                AddressingMode.Indirect => SystemMemory[SystemMemory.ReadTwoBytes(SystemMemory.ReadTwoBytes(CpuRegisters.PC.PostIncrementTwo()))],
+                AddressingMode.IndexedIndirect => SystemMemory[SystemMemory.ReadTwoBytes((byte)(SystemMemory[CpuRegisters.PC++] + CpuRegisters.X))],
+                AddressingMode.IndirectIndexed => SystemMemory[(ushort)(SystemMemory.ReadTwoBytes(SystemMemory[CpuRegisters.PC++]) + CpuRegisters.Y)],
+                _ => throw new ArgumentException($"The given AddressingMode ({mode}) does not have an operand.", nameof(mode))
             };
+        }
+
+        /// <summary>
+        /// Write to an instruction operand for the given addressing mode at the current PC register value.
+        /// PC will be automatically incremented to the start of the next instruction.
+        /// </summary>
+        public void WriteOperand(AddressingMode mode, byte value)
+        {
+            switch (mode)
+            {
+                case AddressingMode.ZeroPage:
+                    SystemMemory[SystemMemory[CpuRegisters.PC++]] = value;
+                    break;
+                case AddressingMode.ZeroPageXIndexed:
+                    SystemMemory[(byte)(SystemMemory[CpuRegisters.PC++] + CpuRegisters.X)] = value;
+                    break;
+                case AddressingMode.ZeroPageYIndexed:
+                    SystemMemory[(byte)(SystemMemory[CpuRegisters.PC++] + CpuRegisters.Y)] = value;
+                    break;
+                case AddressingMode.Relative:
+                    SystemMemory[(ushort)(SystemMemory[CpuRegisters.PC++] + CpuRegisters.PC)] = value;
+                    break;
+                case AddressingMode.Absolute:
+                    SystemMemory[SystemMemory.ReadTwoBytes(CpuRegisters.PC.PostIncrementTwo())] = value;
+                    break;
+                case AddressingMode.AbsoluteXIndexed:
+                    SystemMemory[(ushort)(SystemMemory.ReadTwoBytes(CpuRegisters.PC.PostIncrementTwo()) + CpuRegisters.X)] = value;
+                    break;
+                case AddressingMode.AbsoluteYIndexed:
+                    SystemMemory[(ushort)(SystemMemory.ReadTwoBytes(CpuRegisters.PC.PostIncrementTwo()) + CpuRegisters.Y)] = value;
+                    break;
+                case AddressingMode.Indirect:
+                    SystemMemory[SystemMemory.ReadTwoBytes(SystemMemory.ReadTwoBytes(CpuRegisters.PC.PostIncrementTwo()))] = value;
+                    break;
+                case AddressingMode.IndexedIndirect:
+                    SystemMemory[SystemMemory.ReadTwoBytes((byte)(SystemMemory[CpuRegisters.PC++] + CpuRegisters.X))] = value;
+                    break;
+                case AddressingMode.IndirectIndexed:
+                    SystemMemory[(ushort)(SystemMemory.ReadTwoBytes(SystemMemory[CpuRegisters.PC++]) + CpuRegisters.Y)] = value;
+                    break;
+                default:
+                    throw new ArgumentException($"The given AddressingMode ({mode}) does not have a writeable operand.", nameof(mode));
+            }
+        }
+
+        private void SetFlagsFromAccumulator()
+        {
+            if (CpuRegisters.A == 0)
+            {
+                CpuRegisters.P |= StatusFlags.Zero;
+            }
+            else
+            {
+                CpuRegisters.P &= ~StatusFlags.Zero;
+            }
+
+            if ((CpuRegisters.A & SignBit) == 0)
+            {
+                CpuRegisters.P &= ~StatusFlags.Negative;
+            }
+            else
+            {
+                CpuRegisters.P |= StatusFlags.Negative;
+            }
         }
 
         public static AddressingMode GetAddressingMode(byte instructionGroup, byte addressingModeCode, byte instructionCode)
         {
             return instructionGroup switch
             {
-                // Group 1
+                // Group One
                 0b01 => addressingModeCode switch
                 {
                     0b000 => AddressingMode.IndexedIndirect,
@@ -99,7 +188,7 @@
                     0b111 => AddressingMode.AbsoluteXIndexed,
                     _ => throw new Exception()
                 },
-                // Group 2
+                // Group Two
                 0b10 => addressingModeCode switch
                 {
                     0b000 => AddressingMode.Immediate,
@@ -114,7 +203,7 @@
                     0b111 => AddressingMode.AbsoluteXIndexed,
                     _ => throw new Exception()
                 },
-                // Group 3
+                // Group Three
                 0b00 => addressingModeCode switch
                 {
                     0b000 when instructionCode == 0b001 => AddressingMode.Absolute,
