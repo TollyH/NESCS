dfc75afc1f25248896db3a48eec388ab4dd8560c
TollyH <ptolemyhill@sky.com>
2025-01-10 23:38:51 +0000

    Rename break flag in status flags enum

--- NESCS/Registers.cs
+++ NESCS/Registers.cs
@@ -9,7 +9,7 @@
         Zero = 0b10,
         InterruptDisable = 0b100,
         Decimal = 0b1000,
-        BreakFlag = 0b10000,
+        Break = 0b10000,
         Always = 0b100000,
         Overflow = 0b1000000,
         Negative = 0b10000000
