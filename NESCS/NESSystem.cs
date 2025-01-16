82254c184aac8a0c33c74b28a1db0725f3ddd5bb
TollyH <ptolemyhill@sky.com>
2025-01-16 21:56:04 +0000

    Implement system clock

--- NESCS/NESSystem.cs
+++ NESCS/NESSystem.cs
@@ -1,11 +1,16 @@
-﻿namespace NESCS
+﻿using System.Diagnostics;
+
+namespace NESCS
 {
-    public record struct CycleClock(double ClockHz, double CpuClockDivisor, double PpuClockDivisor, double ApuFrameCounterDivisor);
+    public readonly record struct CycleClock(double ClockHz, int CpuClockDivisor, int PpuClockDivisor, int ApuFrameCounterDivisor)
+    {
+        public readonly long TicksPerClock = (long)((1 / ClockHz) * Stopwatch.Frequency);
+    };
 
     public class NESSystem
     {
         public static readonly CycleClock NtscClockPreset = new(236250000 / 11d, 12, 4, 3937500);  // 60 Hz
-        public static readonly CycleClock PalClockPreset = new(26601712.5, 16, 5, 532034.25);  // 50 Hz
+        public static readonly CycleClock PalClockPreset = new(26601712.5, 16, 5, 532034);  // 50 Hz
 
         public CycleClock CurrentClock { get; set; } = NtscClockPreset;
 
@@ -13,9 +18,28 @@
 
         public readonly Processor CpuCore;
 
+        private int ticksUntilCpuTick = 0;
+
         public NESSystem()
         {
             CpuCore = new Processor(SystemMemory);
+        }
+
+        public void StartClock()
+        {
+            long nextTick = Stopwatch.GetTimestamp() + CurrentClock.TicksPerClock;
+            ClockTick();
+
+            while (Stopwatch.GetTimestamp() < nextTick) { }
+        }
+
+        public void ClockTick()
+        {
+            if (--ticksUntilCpuTick <= 0)
+            {
+                CpuCore.ExecuteClockCycle();
+                ticksUntilCpuTick = CurrentClock.CpuClockDivisor;
+            }
         }
     }
 }
